e5564e8b5d9ab2fb0e3421379cf5c84e26e66506
Raz1ner <63853884+dev-coco@users.noreply.github.com>
2023-11-28 12:17:38 -0400

    Add files via upload

--- exportComment.js
+++ exportComment.js
@@ -8,15 +8,16 @@ function exportComment () {
   let result = '', index = 0
   const commentBlock = document.getElementsByClassName('_3tw__eCCe7j-epNCKGXUKk')
   for (const x of commentBlock) {
-    index++
-    console.log()
-    const userLink = x.querySelector('[data-testid="comment_author_link"]').href
-    const userName = userLink.replace(/.+user\/|\//g, '')
-    const commentDate = x.querySelector('[data-testid="comment_timestamp"]').outerText
-    const vote = x.getElementsByClassName('_1rZYMD_4xY3gRcSS3p8ODO _25IkBM0rRUqWX5ZojEMAFQ _3ChHiOyYyUkpZ_Nm3ZyM2M')[0].outerText
-    let content = x.querySelector('[data-testid="comment"]').outerText
-    if (content.includes('\n')) content = `"${content.replace(/"/g, '""')}"`
-    result += `${index}\t${userName}\t${userLink}\t${commentDate}\t${content}\t${vote}\t${location.href}\n`
+    try {
+      index++
+      const userLink = x.querySelector('[data-testid="comment_author_link"]').href
+      const userName = userLink.replace(/.+user\/|\//g, '')
+      const commentDate = x.querySelector('[data-testid="comment_timestamp"]').outerText
+      const vote = x.getElementsByClassName('_1rZYMD_4xY3gRcSS3p8ODO _25IkBM0rRUqWX5ZojEMAFQ _3ChHiOyYyUkpZ_Nm3ZyM2M')[0].outerText
+      let content = x.querySelector('[data-testid="comment"]').outerText
+      if (content.includes('\n')) content = `"${content.replace(/"/g, '""')}"`
+      result += `${index}\t${userName}\t${userLink}\t${commentDate}\t${content}\t${vote}\t${location.href}\n`
+    } catch {}
   }
   navigator.clipboard.writeText(result).then(() => alert('已复制评论'))
 }
